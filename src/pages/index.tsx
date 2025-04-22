6d7e5529c72d42b2d8217b3f2b0925887a972aa2
Zack McMullins <jsu4478m@gmail.com>
2025-04-21 21:41:11 -0500

    Fix overlay to reveal background image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,14 +7,14 @@ export default function Home() {
         <title>DKC Games - Coming Soon</title>
       </Head>
       <main
-        className="flex min-h-screen flex-col bg-black bg-cover bg-center bg-no-repeat relative"
+        className="relative flex min-h-screen flex-col items-center justify-center bg-cover bg-center bg-no-repeat"
         style={{ backgroundImage: "url('/background.png')" }}
       >
-        {/* Black transparent overlay */}
-        <div className="absolute inset-0 bg-black bg-opacity-70"></div>
+        {/* Black semi-transparent overlay */}
+        <div className="absolute inset-0 bg-black opacity-70 z-0"></div>
 
-        {/* Content above the overlay */}
-        <div className="relative z-10 flex flex-col items-center justify-center flex-1 text-center text-white p-6">
+        {/* Foreground content */}
+        <div className="relative z-10 flex flex-col items-center justify-center text-center text-white p-6">
           <h1 className="text-4xl md:text-6xl font-extrabold mb-4">
             DKC Games - Coming Soon!!!!!!
           </h1>
