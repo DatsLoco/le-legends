fde8221e1191f7c25f34bcc5544fa7855695bf36
Zack McMullins <jsu4478m@gmail.com>
2025-04-21 21:22:31 -0500

    Fix full page background layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,17 +7,18 @@ export default function Home() {
         <title>DKC Games - Coming Soon</title>
       </Head>
       <main
-        className="flex min-h-screen flex-col bg-black bg-cover bg-center relative"
+        className="flex min-h-screen flex-col bg-black bg-cover bg-center bg-no-repeat relative"
         style={{ backgroundImage: "url('/background.png')" }}
       >
-        <div className="w-full bg-black bg-opacity-70 p-6 text-center">
-          <h1 className="text-4xl font-extrabold text-white">
+        {/* Black transparent overlay */}
+        <div className="absolute inset-0 bg-black bg-opacity-70"></div>
+
+        {/* Content above the overlay */}
+        <div className="relative z-10 flex flex-col items-center justify-center flex-1 text-center text-white p-6">
+          <h1 className="text-4xl md:text-6xl font-extrabold mb-4">
             DKC Games - Coming Soon!!!!!!
           </h1>
-        </div>
-
-        <div className="flex flex-1 flex-col items-center justify-center text-white">
-          <p className="text-2xl mt-20">A New Fantasy Experience Awaits...</p>
+          <p className="text-2xl mt-4">A New Fantasy Experience Awaits...</p>
         </div>
       </main>
     </>
