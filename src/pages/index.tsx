2d687b419faead2f76347d633e7b54d221ec8f6e
Zack McMullins <jsu4478m@gmail.com>
2025-04-21 21:46:57 -0500

    Move text to top, force black background

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,18 +7,18 @@ export default function Home() {
         <title>DKC Games - Coming Soon</title>
       </Head>
       <main
-        className="relative flex min-h-screen flex-col items-center justify-center bg-center bg-no-repeat bg-contain"
+        className="relative flex min-h-screen flex-col items-center bg-black bg-center bg-no-repeat bg-contain"
         style={{ backgroundImage: "url('/background.png')" }}
       >
         {/* Black semi-transparent overlay */}
         <div className="absolute inset-0 bg-black opacity-70 z-0"></div>
 
         {/* Foreground content */}
-        <div className="relative z-10 flex flex-col items-center justify-center text-center text-white p-6">
+        <div className="relative z-10 flex flex-col items-center text-center text-white p-6 pt-12">
           <h1 className="text-4xl md:text-6xl font-extrabold mb-4">
             DKC Games - Coming Soon!!!!!!
           </h1>
-          <p className="text-2xl mt-4">A New Fantasy Experience Awaits...</p>
+          <p className="text-2xl mt-2">A New Fantasy Experience Awaits...</p>
         </div>
       </main>
     </>
